5538c07fcc8e6b61bcaae62cf90014ce6013138c
Mohamed Saeed Abdelhafez <52967542+mohammed183@users.noreply.github.com>
2023-04-22 18:31:14 +0200

    Add files via upload

--- Schema.sql
+++ Schema.sql
@@ -1,16 +1,19 @@
 CREATE DATABASE ticketsystem;
+USE ticketsystem;
+-- Creating table to hold tickets
 CREATE TABLE IF NOT EXISTS `tickets` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`title` varchar(255) NOT NULL,
 	`msg` text NOT NULL,
 	`email` varchar(255) NOT NULL,
 	`created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`status` enum('open','closed','resolved') NOT NULL DEFAULT 'open',
+	`resolved` datetime DEFAULT NULL,
+	`status` enum('open','assigned','resolved') NOT NULL DEFAULT 'open',
+	`assignee` int(11) DEFAULT 0,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-INSERT INTO `tickets` (`id`, `title`, `msg`, `email`, `created`, `status`) VALUES (1, 'Test Ticket', 'This is your first ticket.', 'support@codeshack.io', '2020-06-10 13:06:17', 'open');
-
+-- Creating table to hold ticket comments
 CREATE TABLE IF NOT EXISTS `tickets_comments` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`ticket_id` int(11) NOT NULL,
@@ -19,4 +22,14 @@ CREATE TABLE IF NOT EXISTS `tickets_comments` (
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-INSERT INTO `tickets_comments` (`id`, `ticket_id`, `msg`, `created`) VALUES (1, 1, 'This is a test comment.', '2020-06-10 16:23:39');
+
+-- Creating table to keep track of team members, Default is admin to be able to add more team members.
+CREATE TABLE IF NOT EXISTS `team`(
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`name` text NOT NULL,
+	`password` varchar(255) not null,
+	`role` text NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+INSERT INTO `team` (`id`, `name`, `password`,`role`) VALUES (1, 'admin','admin123', 'admin');
